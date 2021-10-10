ff8103a02f4d8130f8299d95f7e44d6d818d6211
Madhuramakrishna <root@ip-172-31-45-207.ap-south-1.compute.internal>
2021-10-10 14:53:48 +0000

    added vpc resource to main.tf file

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-resource "aws_instance" "jha_ec2instance" {
+resource "aws_vpc" "jenkins_vpc" {
+  cidr_block = "10.2.0.0/16"
+  tags = {
+    "Name" = "jenkinsvpc"
+  }
+}
+
+
+resource "aws_instance" "jenkins_ec2instance" {
   ami = "ami-041d6256ed0f2061c"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   availability_zone = "ap-south-1a"
+  count = 2 
+#  key_pair = "general"
 
   tags = {
     name = "jha01"
